fad33f980830a5fdaa58de1b2dca8ce795bb9e90
Ben DeMeyer <ben@bendemeyer.com>
2018-03-18 17:27:53 -0500

    fixed bug where serializer didn't run object level validation

--- keypr/hotel_reservations/serializers.py
+++ keypr/hotel_reservations/serializers.py
@@ -1,11 +1,17 @@
 from hotel_reservations.models import Reservation, HotelConfiguration
 from rest_framework import serializers
+from hotel_reservations.validators import is_valid_date_range, is_available
 
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = "__all__"
+    
+    def validate(self, data):
+        is_valid_date_range(data["check_in"], data["check_out"])
+        is_available(data["check_in"], data["check_out"])
+        return data
 
 
 class HotelConfigurationSerializer(serializers.ModelSerializer):
